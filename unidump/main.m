38693bb6682ac3c9b3dad60554a4a354bbf1c096
Tim Clem <tim.clem@gmail.com>
2017-12-26 20:44:56 -0500

    Initial Commit

---
+++ unidump/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  unidump
+//
+//  Created by Timothy Clem on 12/26/17.
+//  Copyright © 2017 Misterfifths Heavy Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
